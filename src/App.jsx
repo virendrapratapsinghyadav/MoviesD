66fba5ee2356c24bea1ebc1fbca3919c6905632e
Virendra <narveer019@gmail.com>
2025-08-13 18:10:06 +0530

    added cards according to there types

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,29 @@
-import './App.css'
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Header from './components/header/Header';
+import Home from './pages/home/Home';
+import MovieList from './components/movieList/MovieList';
+import MovieDetail from './pages/movieDetail/MovieDetail';
+
+
 
 function App() {
 
   return (
     <>
-      <div>
-        This is movie app
-      </div>
+    <div className="App">
+        <Router>
+          <Header />
+            <Routes>
+                <Route index element={<Home/>}></Route>
+                <Route path="movie/:id" element={<MovieDetail/>}></Route>
+                <Route path="movies/:type" element={<MovieList />}></Route>
+                <Route path="/*" element={<h1>Error Page</h1>}></Route>
+            </Routes>
+        </Router>
+    </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/movieDetail/MovieDetail.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MovieDetail = () => {
+  return (
+    <div>
+      MovieDetailes
+    </div>
+  )
+}
+
+export default MovieDetail
